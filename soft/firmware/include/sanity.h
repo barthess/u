3e0730be5898d12e8c630e34243b2655c82ec6da
barthess <barthess@yandex.ru>
2012-01-26 22:32:09 +0300

    added sanity checker

---
+++ soft/firmware/include/sanity.h
@@ -0,0 +1,8 @@
+#ifndef SANITY_H_
+#define SANITY_H_
+
+
+void SanityControlInit(void);
+
+
+#endif /* SANITY_H_ */
---
+++ soft/firmware/src/sanity.c
@@ -0,0 +1,82 @@
+
+/* Следит за состоянием здоровья органов.*/
+
+#include "ch.h"
+#include "hal.h"
+
+#include "message.h"
+#include "main.h"
+
+#include <mavlink.h>
+#include <common.h>
+#include <bart.h>
+
+/*
+ ******************************************************************************
+ * EXTERNS
+ ******************************************************************************
+ */
+extern uint32_t GlobalFlags;
+//extern RawData raw_data;
+extern Mailbox tolink_mb;
+extern mavlink_heartbeat_t mavlink_heartbeat_struct;
+
+/*
+ ******************************************************************************
+ * DEFINES
+ ******************************************************************************
+ */
+
+/*
+ ******************************************************************************
+ * GLOBAL VARIABLES
+ ******************************************************************************
+ */
+
+
+/*
+ *******************************************************************************
+ *******************************************************************************
+ * LOCAL FUNCTIONS
+ *******************************************************************************
+ *******************************************************************************
+ */
+static WORKING_AREA(SanityControlThreadWA, 256);
+static msg_t SanityControlThread(void *arg) {
+  chRegSetThreadName("Sanity");
+  (void)arg;
+
+  Mail tolink_mail = {NULL, MAVLINK_MSG_ID_HEARTBEAT, NULL};
+
+  while (TRUE) {
+    if (tolink_mail.payload == NULL){
+      tolink_mail.payload = &mavlink_heartbeat_struct;
+      chMBPost(&tolink_mb, (msg_t)&tolink_mail, TIME_IMMEDIATE);
+      palClearPad(GPIOB, GPIOB_LED_R);
+      chThdSleepMilliseconds(100);
+      palSetPad(GPIOB, GPIOB_LED_R);
+      chThdSleepMilliseconds(900);
+    }
+    else
+      GlobalFlags |= POSTAGE_FAILED;
+  }
+  return 0;
+}
+
+
+/*
+ *******************************************************************************
+ * EXPORTED FUNCTIONS
+ *******************************************************************************
+ */
+void SanityControlInit(void){
+
+  mavlink_heartbeat_struct.autopilot = MAV_AUTOPILOT_GENERIC;
+
+  chThdCreateStatic(SanityControlThreadWA,
+          sizeof(SanityControlThreadWA),
+          NORMALPRIO,
+          SanityControlThread,
+          NULL);
+}
+
