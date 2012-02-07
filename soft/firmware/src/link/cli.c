78e315d616a24d606cc050867f251219825699e4
barthess <barthess@yandex.ru>
2012-02-07 17:19:55 +0300

    microrl other example taken as base

--- soft/firmware/src/link/cli.c
+++ soft/firmware/src/link/cli.c
@@ -16,23 +16,6 @@ extern MemoryHeap LinkThdHeap;
 void microrl_set_sigint_callback (microrl_t * this, void (*sigintf)(void));
 
 
-/*
-AVR platform specific implementation routines (for Atmega8, rewrite for your MC)
-*/
-#define _AVR_DEMO_VER "1.0"
-
-// definition commands word
-#define _CMD_HELP   "help"
-#define _CMD_INFO   "info"
-#define _CMD_CLEAR  "clear"
-
-#define _NUM_OF_CMD 3
-
-//available  commands
-char * keyworld [] = {_CMD_HELP, _CMD_INFO, _CMD_CLEAR};
-
-// array for comletion
-char * compl_world [_NUM_OF_CMD + 1];
 
 
 //*****************************************************************************
@@ -52,12 +35,54 @@ char get_char (void){
   return sdGet(shell_sdp);
 }
 
+
+
+
+
+
+
+
+
+
+
+// definition commands word
+#define _CMD_HELP  "help"
+#define _CMD_CLEAR "clear"
+#define _CMD_LIST  "list"
+#define _CMD_LISP  "lisp" // for demonstration completion on 'l + <TAB>'
+#define _CMD_NAME  "name"
+#define _CMD_VER   "version"
+// sub commands for version command
+  #define _SCMD_MRL  "microrl"
+  #define _SCMD_DEMO "demo"
+
+#define _NUM_OF_CMD 6
+#define _NUM_OF_VER_SCMD 2
+
+//available  commands
+char * keyworld [] = {_CMD_HELP, _CMD_CLEAR, _CMD_LIST, _CMD_NAME, _CMD_VER, _CMD_LISP};
+// version subcommands
+char * ver_keyworld [] = {_SCMD_MRL, _SCMD_DEMO};
+
+// array for comletion
+char * compl_world [_NUM_OF_CMD + 1];
+
+// 'name' var for store some string
+#define _NAME_LEN 8
+char name [_NAME_LEN];
+int val;
+
+
 //*****************************************************************************
 void print_help (void)
 {
-  print ("Use TAB key for completion\n\rCommands:\n\r");
-  print ("\tclear               - this help\n\r");
-  print ("\tclear               - clear screen\n\r");
+  print ("Use TAB key for completion\n\rCommand:\n\r");
+  print ("\tversion {microrl | demo} - print version of microrl lib or version of this demo src\n\r");
+  print ("\thelp  - this message\n\r");
+  print ("\tclear - clear screen\n\r");
+  print ("\tlist  - list all commands in tree\n\r");
+  print ("\tname [string] - print 'name' value if no 'string', set name value to 'string' if 'string' present\n\r");
+  print ("\tlisp - dummy command for demonstation auto-completion, while inputed 'l+<TAB>'\n\r");
 }
 
 //*****************************************************************************
@@ -69,18 +94,43 @@ int execute (int argc, const char * const * argv)
   // just iterate through argv word and compare it with your commands
   while (i < argc) {
     if (strcmp (argv[i], _CMD_HELP) == 0) {
-      print ("microrl v");
-      print (MICRORL_LIB_VER);
-      print (" library AVR DEMO v");
-      print (_AVR_DEMO_VER);
-      print("\n\r");
+      print ("microrl library based shell v 1.0\n\r");
       print_help ();        // print help
-    }
-    else if (strcmp (argv[i], _CMD_CLEAR) == 0) {
+    } else if (strcmp (argv[i], _CMD_NAME) == 0) {
+      if ((++i) < argc) { // if value preset
+        if (strlen (argv[i]) < _NAME_LEN) {
+          strcpy (name, argv[i]);
+        } else {
+          print ("name value too long!\n\r");
+        }
+      } else {
+        print (name);
+        print ("\n\r");
+      }
+    } else if (strcmp (argv[i], _CMD_VER) == 0) {
+      if (++i < argc) {
+        if (strcmp (argv[i], _SCMD_DEMO) == 0) {
+          print ("demo v 1.0\n\r");
+        } else if (strcmp (argv[i], _SCMD_MRL) == 0) {
+          print ("microrl v 1.2\n\r");
+        } else {
+          print ((char*)argv[i]);
+          print (" wrong argument, see help\n\r");
+        }
+      } else {
+        print ("version needs 1 parametr, see help\n\r");
+      }
+    } else if (strcmp (argv[i], _CMD_CLEAR) == 0) {
       print ("\033[2J");    // ESC seq for clear entire screen
       print ("\033[H");     // ESC seq for move cursor at left-top corner
-    }
-    else {
+    } else if (strcmp (argv[i], _CMD_LIST) == 0) {
+      print ("available command:\n");// print all command per line
+      for (int i = 0; i < _NUM_OF_CMD; i++) {
+        print ("\t");
+        print (keyworld[i]);
+        print ("\n\r");
+      }
+    } else {
       print ("command: '");
       print ((char*)argv[i]);
       print ("' Not found.\n\r");
@@ -111,8 +161,14 @@ char ** complet (int argc, const char * const * argv)
         compl_world [j++] = keyworld [i];
       }
     }
-  }
-  else { // if there is no token in cmdline, just print all available token
+  } else if ((argc > 1) && (strcmp (argv[0], _CMD_VER)==0)) { // if command needs subcommands
+    // iterate through subcommand for command _CMD_VER array
+    for (int i = 0; i < _NUM_OF_VER_SCMD; i++) {
+      if (strstr (ver_keyworld [i], argv [argc-1]) == ver_keyworld [i]) {
+        compl_world [j++] = ver_keyworld [i];
+      }
+    }
+  } else { // if there is no token in cmdline, just print all available token
     for (; j < _NUM_OF_CMD; j++) {
       compl_world[j] = keyworld [j];
     }
@@ -124,6 +180,7 @@ char ** complet (int argc, const char * const * argv)
   return compl_world;
 }
 #endif
+
 
 //*****************************************************************************
 void sigint (void)
