d0e0a020c7f82b3431a5c504d7240594d42bc414
barthess <barthess@yandex.ru>
2012-08-25 00:08:25 +0300

    fixed 5 point median filter and added 3 point relaization

--- soft/firmware/src/utils.c
+++ soft/firmware/src/utils.c
@@ -107,10 +107,10 @@ void time_test(void){
  *
  * return       median value
  */
-int32_t median_filter_5(int32_t *buf, int32_t sample){
+uint32_t median_filter_5(uint32_t *buf, uint32_t sample){
   const uint32_t N_samples = 5;
   uint32_t j = 0, i = 0;
-  int32_t sorted[5];
+  int32_t sorted[N_samples];
   int32_t tmp;
 
   /* place new sample in fifo */
@@ -135,6 +135,43 @@ int32_t median_filter_5(int32_t *buf, int32_t sample){
     }
   }
 
-  return sorted[3];
+  return sorted[2];
 }
 
+/**
+ * Median filter on 3 points
+ * *buf[in]     pointer to fifo buffer
+ * sample[in]   new sample
+ *
+ * return       median value
+ */
+uint32_t median_filter_3(uint32_t *buf, uint32_t sample){
+  const uint32_t N_samples = 3;
+  uint32_t j = 0, i = 0;
+  int32_t sorted[N_samples];
+  int32_t tmp;
+
+  /* place new sample in fifo */
+  for(j=1; j<N_samples; j++){
+    buf[j-1] = buf[j];
+  }
+  buf[j-1] = sample;
+
+  /* place data in temporal buffer */
+  for(j=0; j<N_samples; j++){
+    sorted[j] = buf[j];
+  }
+
+  /* booble sort */
+  for(i=0; i<=N_samples-1; i++){
+    for(j=i+1; j<N_samples;j++){
+      if(sorted[i] > sorted[j]){
+        tmp = sorted[i];
+        sorted[i] = sorted[j];
+        sorted[j] = tmp;
+      }
+    }
+  }
+
+  return sorted[1];
+}
