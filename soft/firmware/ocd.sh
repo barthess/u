e9961c9621085614328bf06ed04a49dfddb4065c
barthess <barthess@yandex.ru>
2012-09-08 15:06:40 +0000

    nop

--- soft/firmware/ocd.sh
+++ soft/firmware/ocd.sh
@@ -3,6 +3,6 @@
 openocd -c "gdb_memory_map enable" \
 	-c "gdb_flash_program enable" \
 	-c "reset_config trst_and_srst" \
-	-c "log_output openocd.log" \
 	-f /usr/share/openocd/scripts/interface/olimex-arm-usb-ocd.cfg \
-	-f /usr/share/openocd/scripts/target/stm32f4x.cfg
+	-f ./stm32f4x.cfg \
+	#-c "log_output openocd.log" \
