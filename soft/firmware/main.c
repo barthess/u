3bf4fb6b2a9b422bac648fe62c87a19aa4dbb6c0
barthess <barthess@yandex.ru>
2012-09-18 13:17:53 +0000

    cleaned done todo

--- soft/firmware/main.c
+++ soft/firmware/main.c
@@ -3,8 +3,6 @@
  * cmpilation without -fomit-frame-pointer cause stack overflows.
  */
 
-// TODO: usage of local ned notation and GPS.
-// TODO: before waypoints ACK message there must be much deeper parameter checks.
 // TODO: speed autosetting
 // TODO: combine barometer and accelerometer in one filter.
 // TODO: (semi)automated zeroing of accelerometer.
