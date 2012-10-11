75a99bb26525dd6bcdcba0f0fa3c6fd4dd7d9e98
barthess <barthess@yandex.ru>
2012-10-11 22:05:59 +0300

    calculated some files with constants for different SYS_CLK values

---
+++ soft/firmware/PLL_168MHz.h
@@ -0,0 +1,4 @@
+#define STM32_PLLM_VALUE                    4
+#define STM32_PLLN_VALUE                    168
+#define STM32_PLLP_VALUE                    2
+#define STM32_PLLQ_VALUE                    7
