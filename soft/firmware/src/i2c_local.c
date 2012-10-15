ecdb99769a68023abd44be231dc5c1a60eef1d4c
barthess <barthess@yandex.ru>
2012-10-15 13:24:46 +0300

    stable catching of bug

--- soft/firmware/src/i2c_local.c
+++ soft/firmware/src/i2c_local.c
@@ -21,8 +21,12 @@
 /* interface #2 */
 static const I2CConfig i2cfg2 = {
     OPMODE_I2C,
-    400000, //100000, //
-    FAST_DUTY_CYCLE_2, //STD_DUTY_CYCLE, //
+    400000,
+    FAST_DUTY_CYCLE_2,
+//    100000,
+//    STD_DUTY_CYCLE
+//    360000,
+//    FAST_DUTY_CYCLE_16_9,
 };
 
 /*
