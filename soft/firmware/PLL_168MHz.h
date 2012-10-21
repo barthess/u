2e6ebc486cecc6c64c2995399042901a2a0fa7eb
barthess <barthess@yandex.ru>
2012-10-22 02:33:55 +0300

    deleted old code

--- soft/firmware/PLL_168MHz.h
+++ soft/firmware/PLL_168MHz.h
@@ -1,8 +1,3 @@
-//#define STM32_PLLM_VALUE                    4
-//#define STM32_PLLN_VALUE                    168
-//#define STM32_PLLP_VALUE                    2
-//#define STM32_PLLQ_VALUE                    2
-
 #define STM32_PLLM_VALUE                    8
 #define STM32_PLLN_VALUE                    336
 #define STM32_PLLP_VALUE                    2
