7643006903f07a75257a2507cd1df853f3af83a3
barthess <barthess@yandex.ru>
2012-10-22 02:34:23 +0300

    added 64MHz setting file for PLL

---
+++ soft/firmware/PLL_64MHz.h
@@ -0,0 +1,6 @@
+#define STM32_PLLM_VALUE                    8
+#define STM32_PLLN_VALUE                    64
+#define STM32_PLLP_VALUE                    2
+#define STM32_PLLQ_VALUE                    2
+
+#define STM32_VOS                           STM32_VOS_LOW
