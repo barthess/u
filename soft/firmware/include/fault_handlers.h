72dd860365aade3eb851f8806780d135661c2768
barthess <barthess@yandex.ru>
2012-10-28 14:31:18 +0300

    fixed fast boot logic

--- soft/firmware/include/fault_handlers.h
+++ soft/firmware/include/fault_handlers.h
@@ -2,6 +2,12 @@
 #define PANIC_H_
 
 /**
+ * Evaluates to TRUE if system runs under debugger control.
+ * Note. This bit resets only by power reset.
+ */
+#define is_under_debugger()   (((CoreDebug)->DHCSR) & CoreDebug_DHCSR_C_DEBUGEN_Msk)
+
+/**
  * evaluates to TRUE if system boots after soft reset cause by SYSRESETREQ
  */
 #define was_softreset()     (((RCC)->CSR) & RCC_CSR_SFTRSTF)
@@ -14,19 +20,13 @@
 /**
  * Determine need of full initialization procedure logic
  */
-//#define need_full_init()    (((RCC)->CSR) & (RCC_CSR_SFTRSTF || RCC_CSR_PADRSTF))
-#define need_full_init()    (((RCC)->CSR) & RCC_CSR_SFTRSTF)
+//#define need_full_init()    (!(((RCC)->CSR) & (RCC_CSR_SFTRSTF || RCC_CSR_PADRSTF)))
+#define need_full_init()      (!(((RCC)->CSR) & RCC_CSR_SFTRSTF))
 
 /**
  * clear all reset flags
  */
 #define clear_reset_flags()   (((RCC)->CSR) |= RCC_CSR_RMVF)
-
-/**
- * Evaluates to TRUE if system runs under debugger control.
- * Note. This bit resets only by power reset.
- */
-#define is_under_debugger()   (((CoreDebug)->DHCSR) & CoreDebug_DHCSR_C_DEBUGEN_Msk)
 
 
 void uav_panic_handler(void);
