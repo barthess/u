438e183f1ed07698fb64f2cd04862f96a80571b5
barthess <barthess@yandex.ru>
2012-10-29 09:51:08 +0300

    nop

--- soft/firmware/uav.h
+++ soft/firmware/uav.h
@@ -21,6 +21,7 @@
 #include "dcm_cli.h"
 #include "dsp.h"
 #include "eeprom.h"
+#include "eeprom_conf.h"
 #include "exti_local.h"
 #include "fixed_wing.h"
 #include "geometry.h"
@@ -77,10 +78,6 @@
 #define LINKSD  SD2
 #define GPSSD   SD1
 #define SHELLSD LINKSD
-
-/******************************************************************
- * build test suit for EEPROM abstraction layer */
-#define USE_EEPROM_TEST_SUIT    FALSE
 
 /******************************************************************
  * Disarm halting on panic and changing it to soft reset after this amount of time */
@@ -191,15 +188,6 @@ typedef struct GlobalFlags_t{
 #if (CH_FREQUENCY) >= 1000
 #define TIME_BOOT_MS ((chTimeNow()) / ((CH_FREQUENCY) / 1000))
 #endif
-
-
-/******************************************************************/
-#define EEPROM_PAGE_SIZE      128         /* page size in bytes. Consult datasheet. */
-#define EEPROM_SIZE           65536       /* total amount of memory in bytes */
-#define EEPROM_I2CD           I2CD2       /* ChibiOS I2C driver used to communicate with EEPROM */
-#define EEPROM_I2C_ADDR       0b1010000   /* EEPROM address on bus */
-#define EEPROM_WRITE_TIME_MS  20          /* time to write one page in mS. Consult datasheet! */
-#define EEPROM_TX_DEPTH       (EEPROM_PAGE_SIZE + 2)/* temporal transmit buffer depth for eeprom driver */
 
 /* region for settings file */
 #define EEPROM_SETTINGS_START         8192
