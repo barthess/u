12f6b12059235300e89abe0e93f210c99aa5dc0b
barthess <barthess@yandex.ru>
2013-04-20 13:43:31 +0300

    first code in wp database

--- soft/firmware/src/controller/waypoint_db.cpp
+++ soft/firmware/src/controller/waypoint_db.cpp
@@ -1,25 +1,127 @@
 #include "uav.h"
 #include "message.hpp"
+#include "waypoint_db.hpp"
+
 
 void save_waypoint_count(int i){
   (void)i;
   chDbgPanic("temporal stub");
 }
-
 bool get_waypoint_from_eeprom(int a, mavlink_mission_item_t *b){
   (void)a;
   (void)b;
   chDbgPanic("temporal stub");
   return false;
 }
-
 int get_waypoint_count(void){
   chDbgPanic("temporal stub");
   return 0;
 }
-
 void save_waypoint_to_eeprom(int a, mavlink_mission_item_t *b){
     (void)a;
   (void)b;
   chDbgPanic("temporal stub");
 }
+
+/*
+ ******************************************************************************
+ * DEFINES
+ ******************************************************************************
+ */
+
+/*
+ ******************************************************************************
+ * EXTERNS
+ ******************************************************************************
+ */
+
+/*
+ ******************************************************************************
+ * PROTOTYPES
+ ******************************************************************************
+ */
+
+/*
+ ******************************************************************************
+ * GLOBAL VARIABLES
+ ******************************************************************************
+ */
+
+/*
+ ******************************************************************************
+ ******************************************************************************
+ * LOCAL FUNCTIONS
+ ******************************************************************************
+ ******************************************************************************
+ */
+
+/*
+ ******************************************************************************
+ * EXPORTED FUNCTIONS
+ ******************************************************************************
+ */
+/**
+ *
+ */
+WpDB::WpDB(void){
+  this->dbfile = NULL;
+}
+
+/**
+ * file must be allready opened
+ */
+uint16_t WpDB::connect(EepromFile *dbfile){
+  this->dbfile = dbfile;
+  dbfile->setPosition(0);
+  count = dbfile->readHalfWord();
+  return count;
+}
+
+
+bool WpDB::get(uint16_t seq, mavlink_mission_item_t *wpp){
+  chDbgCheck((NULL != this->dbfile), "DB unconnected");
+  chDbgCheck((NULL != wpp), "");
+  if (seq > count)
+    return CH_FAILED;
+  else{
+
+  }
+}
+
+
+
+bool WpDB::massErase(void){
+  return CH_FAILED;
+}
+
+
+
+bool WpDB::deleteAll(void){
+  return CH_FAILED;
+}
+
+
+
+bool WpDB::write(uint16_t n, mavlink_mission_item_t *wpp){
+return CH_FAILED;
+}
+
+
+
+uint16_t WpDB::len(void){
+  return 0;
+}
+
+
+
+bool WpDB::validate(mavlink_mission_item_t *wpp){
+  return CH_FAILED;
+}
+
+
+
+bool WpDB::finalize(void){
+  return CH_FAILED;
+}
+
+
--- soft/firmware/src/controller/waypoint_db.hpp
+++ soft/firmware/src/controller/waypoint_db.hpp
@@ -1,9 +1,35 @@
 #ifndef WAYPOINT_DB_HPP_
 #define WAYPOINT_DB_HPP_
 
+#include "eeprom_file.hpp"
+
 void save_waypoint_count(int i);
 bool get_waypoint_from_eeprom(int a, mavlink_mission_item_t *b);
 int get_waypoint_count(void);
 void save_waypoint_to_eeprom(int a, mavlink_mission_item_t *b);
+
+/*
+ * DB structure:
+ * uint16_t                 waypoint count currently stored in DB
+ * mavlink_mission_item_t[] array of fixed size chunks storing wp data.
+ *                          Chunks must be stored subsequently in waypoints' order
+ */
+class WpDB{
+public:
+  WpDB(void);
+  uint16_t connect(EepromFile *dbfile);
+  bool get(uint16_t seq, mavlink_mission_item_t *wpp);
+  bool massErase(void);
+  bool deleteAll(void);
+  bool write(uint16_t n, mavlink_mission_item_t *wpp);
+  uint16_t len(void);
+
+private:
+  EepromFile *dbfile;
+  bool validate(mavlink_mission_item_t *wpp);
+  bool finalize(void);
+  uint16_t count;
+  uint8_t eeprombuf[sizeof(mavlink_mission_item_t)];
+};
 
 #endif /* WAYPOINT_DB_HPP_ */
